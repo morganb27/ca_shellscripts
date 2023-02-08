06dc7ae300010c44c646d591a50d22a14ee704c4
morganb27 <115886162+morganb27@users.noreply.github.com>
2023-02-08 11:13:45 +0100

    Add files via upload

---
+++ variables&input.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#this a comment 
+echo "Hello World" #this is also a comment
+
+#BIRTHDATE="Jan 1, 2000"
+#Presents=10
+#BIRTHDAY=$(date -d "$BIRTHDATE" +%A)
+
+
+#read -p 'username : ' user_var
+#read -sp 'password : ' pass_var
+#echo
+#echo "username : $user_var"
+#echo "password : $pass_var"
+
+#echo "Enter names : "
+#read -a names
+#echo "Names : ${names[0]}, ${names[1]}"
+
+#echo "Enter name : "
+#read
+#echo "Name : $REPLY"
+
+
+
+
+
+
+
+
+
