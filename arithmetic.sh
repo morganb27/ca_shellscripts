9c2a536f6aff7e67cb98af85094f0eddb5063313
morganb27 <115886162+morganb27@users.noreply.github.com>
2023-02-08 14:40:37 +0100

    Add files via upload

---
+++ arithmetic.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+num1=21
+num2=5
+
+echo $(( num1 + num2 ))
+echo $(( num1 - num2 ))
+echo $(( num1 * num2 ))
+echo $(( num1 / num2 ))
+echo $(( num1 % num2 )) #what remains after division
+
+
+echo $( expr $num1 + $num2 )
+echo $( expr $num1 - $num2 )
+echo $( expr $num1 \* $num2 )
+echo $( expr $num1 / $num2 )
+echo $( expr $num1 % $num2 ) #what remains after division
+
