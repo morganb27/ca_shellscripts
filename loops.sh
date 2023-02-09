587084e85e9d5d0214a208272d801ec4f89fcda5
morganb27 <115886162+morganb27@users.noreply.github.com>
2023-02-09 09:21:40 +0100

    Add files via upload

---
+++ loops.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+echo "While loops"
+
+#n=1
+
+#while [ $n -le 10 ]
+#do 
+#    echo "$n"
+#    n=$(( ++n ))
+#    sleep 1
+#done
+
+echo "Open terminal"
+
+#n=1
+
+#while [ $n -le 3 ]
+#do 
+#    echo "$n"
+#    n=$(( ++n ))
+#    exec open -a Terminal.app &
+#done
+
+#while read p
+#do
+#    echo $p
+
+#done < Shell/hello.sh
+
+#cat Shell/hello.sh | while read p
+#do 
+#    echo $p
+#done
+
+#while IFS= read -r line
+##do
+#    echo $line
+#done < Shell/hello.sh
+
+#n=1
+
+#until [ $n -gt 10 ]
+#do
+#    echo $n
+#    n=$(( n+1 ))
+#done
+
+
+
+
+
+
+
+
+
+
+
